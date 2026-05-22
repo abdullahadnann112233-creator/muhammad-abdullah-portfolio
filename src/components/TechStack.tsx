dcf5f308eff908b6bdfa2f7eba8ec2c410f3db5d
Muhammad Abdullah <abdullahadnann112233@gmail.com>
2026-05-23 00:39:27 +0500

    hi

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -167,15 +167,15 @@ const TechStack = () => {
   }, []);
 
   return (
-    <div className="techstack">
-      <h2> Techstack</h2>
+    <div className="stack">
+      <h2> stack</h2>
 
       <Canvas
         shadows
         gl={{ alpha: true, stencil: false, depth: false, antialias: false }}
         camera={{ position: [0, 0, 20], fov: 32.5, near: 1, far: 100 }}
         onCreated={(state) => (state.gl.toneMappingExposure = 1.5)}
-        className="tech-canvas"
+        className="-canvas"
       >
         <ambientLight intensity={1} />
         <spotLight
