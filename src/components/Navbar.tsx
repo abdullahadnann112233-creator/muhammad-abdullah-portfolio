17a38c218f5653b540f1a5249899b226472de4dc
Muhammad Abdullah <abdullahadnann112233@gmail.com>
2026-05-23 22:37:03 +0500

    hi

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -43,7 +43,7 @@ const Navbar = () => {
     <>
       <div className="header">
         <a href="/#" className="navbar-title" data-cursor="disable">
-          AM
+          MA
         </a>
         <a
           href="https://www.linkedin.com/in/muhammad-abdullah-055067386/"
